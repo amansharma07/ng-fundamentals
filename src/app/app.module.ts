71fd2794ebae6d7b3b54d7a2ca2199316604bc08
Aman Sharma <amansharma1829@gmail.com>
2021-07-22 19:35:34 +0530

    Add and update files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { Eventcomponent } from './app.Eventcomponent';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    Eventcomponent
+  ],
+  bootstrap: [Eventcomponent]
 })
 export class AppModule { }
